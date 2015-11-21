e412e12a082c0802baaeda478f89978afe5a5da5
PrasunPK <prasun_1993@rediffmail.com>
2015-11-21 11:28:07 +0530

    Two constructors 'Team' & 'Player' are added and primarily tested.

--- teamFormation.js
+++ teamFormation.js
@@ -1,2 +1,14 @@
 var teamLib = {};
 exports.teamLib = teamLib;
+
+teamLib.Team = function(players){
+	this.team1 = {};
+	this.team2 = {};
+};
+
+teamLib.Player = function(){
+	this.id = function(){};
+	this.firstHand = [];
+	this.secondHand = [];
+	this.hasPair = false;
+};
--- test/teamTest.js
+++ test/teamTest.js
@@ -4,9 +4,16 @@ var assert = chai.assert;
 var expect = chai.expect;
 
 describe('Team',function(){
-	it('creates teams with two players');
+	it('creates teams with two players',function(){
+		var players = ['Ramu','Haru','Shibu','Piku'];
+		var team = new teamModule.Team(players);
+		expect(team).to.have.all.keys('team1','team2');
+	});
 });
 
-describe('MakePlayer',function(){
-	it('creates players with properties');
+describe('Player',function(){
+	it('creates players with properties',function(){
+		var player = new teamModule.Player();
+		expect(player).to.have.all.keys('id','firstHand','secondHand','hasPair');
+	});
 });
