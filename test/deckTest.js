066aebbce5f2fb1562c983431426be515558448b
rahulna <rahulna@INrahulna.local>
2015-11-21 18:10:37 +0530

    Grouped testing of property and identity in Card

--- test/deckTest.js
+++ test/deckTest.js
@@ -4,57 +4,43 @@ var assert = chai.assert;
 var expect = chai.expect;
 
 describe('Card',function(){
-	describe('property',function(){
-		it('has suit',function(){
-			assert.property(new m.Card('A','Diamond'),'suit');
-		});
-		it('has name',function(){
-			assert.property(new m.Card('K','Spade'),'name');
-		});
-		it('has rank',function(){
-			assert.property(new m.Card('Q','Club'),'rank');
-		});
-		it('has point',function(){
-			assert.property(new m.Card('J','Heart'),'point');
+	describe('Property',function(){
+		it('has suit, name, rank, point',function(){
+			expect(new m.Card('A','Diamond')).to.have.all.keys('suit','name','rank','point');
 		});
 	});
-	describe('identity',function(){
-		it('is A of Diamond',function(){
-			var expected = {
-				name : 'A',
-				suit : 'Diamond',
-				point : 1,
-				rank : 3
-			};
-			assert.deepEqual(new m.Card('A','Diamond'),expected);
-		});
-		it('is K of Spade',function(){
-			var expected = {
-				name : 'K',
-				suit : 'Spade',
-				point : 0,
-				rank : 5
-			};
-			assert.deepEqual(new m.Card('K','Spade'),expected);
-		});
-		it('is Q of Club',function(){
-			var expected = {
+	it('is given with its rank and point according to its name',function(){
+		var diamondA = {
+			name : 'A',
+			suit : 'Diamond',
+			point : 1,
+			rank : 3
+		};
+		assert.deepEqual(new m.Card('A','Diamond'),diamondA);
+		
+		var spadeK = {
+			name : 'K',
+			suit : 'Spade',
+			point : 0,
+			rank : 5
+		};
+		assert.deepEqual(new m.Card('K','Spade'),spadeK);
+
+		var clubQ = {
 				name : 'Q',
 				suit : 'Club',
 				point : 0,
 				rank : 6
 			};
-			assert.deepEqual(new m.Card('Q','Club'),expected);
-		});
-		it('is J of Heart',function(){
-			var expected = {
+		assert.deepEqual(new m.Card('Q','Club'),clubQ);
+
+		var heartJ = {
 				name : 'J',
 				suit : 'Heart',
 				point : 3,
 				rank : 1
 			};
-			assert.deepEqual(new m.Card('J','Heart'),expected);
-		});
+		assert.deepEqual(new m.Card('J','Heart'),heartJ);
 	});
 });
 describe('Deck',function(){
