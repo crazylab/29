01e2cecfc8f1a46ea80e77f17d4aaae3fe1d180f
PrasunPK <prasun_1993@rediffmail.com>
2015-11-24 14:10:59 +0530

    [Brindaban/Prasun] Added the feature of setting ID and name for players
    What changes have you made?
    
    * Added a new function (setIdAndNames) for setting the ID and names.
    
    Why do we need this change?
    
    *Each player must have ID for participating in a game. So creating ID is necessary.
    
    What can this change impact?
    
    *It can help the croupier to generate teams with players and their unique IDs.

--- croupier.js
+++ croupier.js
@@ -1,11 +1,27 @@
 var deckLib = require('./deck.js').m;
 var teamLib = require('./teamFormation.js').team;
+var ld = require('lodash');
 
 var croupier = {};
 exports.croupier = croupier;
 
-croupier.getShuffledCards = function(){
+croupier.getShuffledDeck = function(){
 	var deck = new deckLib.Deck();
 	var shuffledDeck = deck.shuffle(deck.cards);
 	return shuffledDeck;
+};
+
+var makeTeams = function(playerNames,ids){
+	var team_1 = new teamLib.Team({name:playerNames[0], id:ids[0]},
+									{name:playerNames[2], id:ids[2]});
+	var team_2 = new teamLib.Team({name:playerNames[1], id:ids[1]},
+									{name:playerNames[3], id:ids[3]});
+	return {team_1:team_1, team_2:team_2};
+};
+
+croupier.setIdAndNames = function(playerNames){
+	var ids = [];
+	for(var i = 0; i < 4; i++)
+		ids.push(ld.uniqueId());
+	return makeTeams(playerNames,ids);
 };
--- test/croupierTest.js
+++ test/croupierTest.js
@@ -4,11 +4,34 @@ var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 
-describe('shuffle',function(){
+describe('getShuffledDeck',function(){
+	var shuffledDeck = m.getShuffledDeck();
 	it('gives the deck with shuffle property',function(){
-		expect(m.getShuffledCards()).to.have.all.keys('cards');
+		expect(shuffledDeck).to.have.all.keys('cards');
 	});
-	it('will get 32 cards after shuffling',function(){
-		expect(m.getShuffledCards().cards).to.have.length(32);
+	describe('shuffle',function(){
+		it('will get 32 cards after shuffling',function(){
+			expect(shuffledDeck.cards).to.have.length(32);
+		});
 	});
-})
+});
+
+describe('setIdAndNames',function(){
+	var playerNames = ['Ramu','Manu','Ram','Hari'];
+	var playersWithID = m.setIdAndNames(playerNames);
+	it('sets id for each player and asserts name to each',function(){
+		expect(playersWithID).to.have.all.keys('team_1','team_2');
+	});
+	describe('player\'s id',function(){
+		var team1 = playersWithID.team_1;
+		it('is present for each player',function(){
+			expect(team1.player_1).to.have.property('id');
+			expect(team1.player_2).to.have.property('id');
+			assert.equal(team1.player_2.name,'Ram');
+		});
+		it('also takes care of making the IDs unique',function(){
+			assert.notEqual(team1.player_2.id,playersWithID.team_2.player_2.id);
+		});
+	});
+
+});
