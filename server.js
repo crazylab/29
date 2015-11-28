e9cfe3a6499a304278ee0181ba2d01e4922bd194
rahulna <rahulna@INrahulna.local>
2015-11-28 11:05:54 +0530

    [Rahul/Prasun] Added server to serve static file
    What changes have you made?
    
    * Added basic structure of the game page with css
    
    Why do we need this change?
    
    *To start working over stories and see the effects
    
    What can this change impact?
    
    *We can manually test our stories as a player's prospective

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 var http = require('http');
+var fs = require('fs');
+var players = [];
 
-var handleRequest = function(req, res){
-	
+var serveStaticFile = function(req,res){
+	if(req.url === '/')
+		req.url = '/index.html';
+	var path = './public' + req.url;
+	fs.readFile(path, function(err,data){
+		if(data){
+			res.statusCode = 200;
+			console.log(res.statusCode);
+			res.end(data);
+		}
+		else{
+			res.statusCode = 404;
+			console.log(res.statusCode);
+			res.end('Not Found');
+		}
+	});
+}
+var handle_get = function(req,res){
+	serveStaticFile(req,res);
+}
+var handle_post = function(req,res){
+
+}
+var method_not_allowed = function(req,res){
+	res.statusCode = 405;
+	console.log(res.statusCode);
+	res.end('Method Not Allowed');
+}
+var requestHandler = function(req, res){
+	var ip = req.connection.remoteAddress;
+	console.log('user request: ',req.url);
+	if(req.method === 'GET')
+		handle_get(req,res);
+	else if(req.method === 'POST')
+		handle_post(req,res);
+	else
+		method_not_allowed(req,res);
 };
 
-var server = http.createServer(handleRequest);
-server.listen(7000, function(){
-	console.log('listening ... ');
-});
+http.createServer(requestHandler).listen(8000);;
