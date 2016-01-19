6c087a9b3545c7f37d383f84f005586503b1103b
Rahul <rahulna@thoughtworks.com>
2016-01-19 12:52:30 +0530

    [Rahul/Brindaban] - Updated server and package.json for hosting
    What changes have you made?
    
    *Added engines in package.json
    Modified server port to support hosting in openshift
    
    Why do we need this change?
    
    *To host the game in openshift
    
    What can this change impact?
    
    *Game can be hosted in openshift server.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require('http');
-var Game = require('./lib/game');
-var GameController = require('./lib/gameController');
-var createGame = require('./lib/createGame');
+var Game = require('./lib/game.js');
+var GameController = require('./lib/gameController.js');
+var createGame = require('./lib/createGame.js');
 
 var controller = GameController(createGame());
-const PORT = 3456;
-http.createServer(controller).listen(PORT).on('error',function(err){
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+http.createServer(controller).listen(port, ipaddress).on('error',function(err){
 	console.log(err.message);
 });
